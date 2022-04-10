94b6a2fc4f89d252b4d09c80485cee676e896476
OmegaWei1984 <omegawei1984@gmail.com>
2022-04-10 20:14:33 +0800

    add containers/associative

---
+++ practise/containers/associative.cpp
@@ -0,0 +1,31 @@
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    set<int> s;
+    s.insert(0);
+    s.insert(1);
+    s.insert(1);
+    s.insert(2);
+    assert(s.size() == 3);
+
+    map<string, int> m = {{"a", 0}, {"b", 1}, {"c", 2}};
+    assert(m.size() == 3);
+    m["d"] = 3;
+    assert(m.size() == 4);
+    m.erase("d");
+    assert(m.size() == 3);
+
+    unordered_set<int> us = {0, 1, 2};
+    assert(us.size() == 3);
+
+    unordered_map<string, int> um = {{"a", 0}, {"b", 1}, {"c", 2}};
+    assert(um.size() == 3);
+
+    return 0;
+}
