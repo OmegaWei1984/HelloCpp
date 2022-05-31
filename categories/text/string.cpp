5db45c3f16d8cea82f0e49204fd0cd42099eb46f
OmegaWei1984 <omegawei1984@gmail.com>
2022-05-31 22:14:07 +0800

    add split in text/string

--- categories/text/string.cpp
+++ categories/text/string.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cassert>
+#include <regex>
 
 using namespace std;
+
+// before C++11
+void split(const string& s, vector<string>& tokens, const string& delimiters = " ")
+{
+    string::size_type lastPos = s.find_first_not_of(delimiters, 0);
+    string::size_type pos = s.find_first_of(delimiters, lastPos);
+    while (string::npos != pos || string::npos != lastPos) {
+        tokens.push_back(s.substr(lastPos, pos - lastPos)); //use emplace_back after C++11
+        lastPos = s.find_first_not_of(delimiters, pos);
+        pos = s.find_first_of(delimiters, lastPos);
+    }
+}
 
 int main(void) {
     string str = "abc";
@@ -26,8 +41,22 @@ int main(void) {
     cout << str4 << endl;
 
     assert(stoi("42") == 42);
-    assert(stof("3.1415926") == 3.1415926);
+    float f_pi = 3.1415926;
+    assert(stof("3.1415926") == f_pi);
+    // assert(stof("3.1415926") == 3.1415926);
     assert(to_string(42) == "42");
+
+    string sentence = "the quick brown fox jumps over the lazy dog";
+    vector<string> tokens;
+    split(sentence, tokens);
+    for (auto &v : tokens)
+        cout << v << endl;
+
+    regex ws_re("\\s+");
+    vector<string> v(sregex_token_iterator(sentence.begin(), sentence.end(), ws_re, -1), 
+    sregex_token_iterator());
+    for (auto &&s : v)
+        cout << s << endl;
 
     return 0;
 }
