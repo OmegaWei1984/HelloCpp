36f65c6fb52251a813351a53cf7e156ed9a9f2a8
OmegaWei1984 <omegawei1984@gmail.com>
2022-09-20 21:59:41 +0800

    新增 basic/value_category

---
+++ basic/value_category.cpp
@@ -0,0 +1,11 @@
+/**
+ *                    expression
+ *                        │  │
+ *                        │  │
+ *           glvalue ◄────┘  └────► rvalue
+ *             │ │                     │ │
+ *             │ │                     │ │
+ * lvalue ◄────┘ └─────► xvalue ◄──────┘ └────► prvalue
+ * 
+ */
+
