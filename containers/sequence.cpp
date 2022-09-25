fd3bf1d80b640c0f857abffb65fa237c1b229893
OmegaWei1984 <omegawei1984@gmail.com>
2022-09-26 01:09:24 +0800

    vector 元素不抛异常的移动构造函数的例子

--- containers/sequence.cpp
+++ containers/sequence.cpp
@@ -4,10 +4,32 @@
 #include <deque>
 #include <forward_list>
 #include <list>
+#include <cassert>
 
 using namespace std;
 
-int main(void) {
+class Obj1
+{
+public:
+    Obj1() { cout << "Obj1()" << endl; }
+
+    Obj1(const Obj1 &) { cout << "Obj1(const Obj1&)" << endl; }
+
+    Obj1(Obj1 &&) { cout << "Obj1(Obj1&&)" << endl; }
+};
+
+class Obj2
+{
+public:
+    Obj2() { cout << "Obj2()" << endl; }
+
+    Obj2(const Obj2 &) { cout << "Obj2(const Obj2&)" << endl; }
+
+    Obj2(Obj2 &&) noexcept { cout << "Obj2(Obj2&&)" << endl; }
+};
+
+int main(void)
+{
     array<int, 2> arr;
     assert(arr.size() == 2);
 
@@ -22,16 +44,29 @@ int main(void) {
     assert(deq.size() == 2);
 
     forward_list<int> fl(5);
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         fl.push_front(i);
     }
-    
+
     list<int> l(1);
     l.push_front(-1);
     l.push_back(2);
     auto lit = find(l.cbegin(), l.cend(), 2);
     l.insert(lit, 1);
     assert(l.size() == 4);
+
+    vector<Obj1> v1;
+    v1.reserve(2);
+    v1.emplace_back();
+    v1.emplace_back();
+    v1.emplace_back();
+
+    vector<Obj2> v2;
+    v2.reserve(2);
+    v2.emplace_back();
+    v2.emplace_back();
+    v2.emplace_back();
 
     return 0;
 }
